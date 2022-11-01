93d58fc1f99bcdaf9e70f230e1e11fafcf6f9e92
TienTran1504 <74586033+TienTran1504@users.noreply.github.com>
2022-11-01 19:48:01 +0700

    Header(part 1)

--- src/components/Layout/DefaultLayout/Header/index.js
+++ src/components/Layout/DefaultLayout/Header/index.js
@@ -1,5 +1,21 @@
+import './style.css'
 function Header() {
-    return <h2>Header</h2>;
+    return (
+        <div className="header">
+            <span class="label">
+                <span className="active">E</span>FOOD
+            </span>
+
+            <ul className="menu-list">
+                <li className="menu-item active">Home</li>
+                <li className="menu-item ">Menu</li>
+                <li className="menu-item ">Service</li>
+                <li className="menu-item ">Contact</li>
+            </ul>
+
+        </div>
+
+    );
 }
 
 export default Header;
