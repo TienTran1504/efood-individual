4972587c84bcada117de04d3603533d36bc8f783
TienTran1504 <74586033+TienTran1504@users.noreply.github.com>
2022-11-03 19:41:58 +0700

    Header search result

--- src/components/Layout/DefaultLayout/Header/index.js
+++ src/components/Layout/DefaultLayout/Header/index.js
@@ -1,9 +1,20 @@
 import classes from './Header.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
-
+import Tippy from '@tippyjs/react/headless';
+import { useEffect, useState } from 'react';
+import { Wrapper as PopperWrapper } from '~/components/Popper';
+import FoodItem from '~/components/FoodItem';
 
 function Header() {
+    const [searchResult, setSearchResult] = useState([]);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setSearchResult([1]);
+        }, 0)
+    }, [])
+
     return (
         <div className={classes.wrapper}>
 
@@ -12,19 +23,37 @@ function Header() {
                     <div className={classes.active}>E</div>FOOD
                 </div>
 
-                <div className={classes.search}>
-                    <input placeholder="Search dishes..." spellCheck={false} />
-                    <button className={classes.clear}>
-                        <FontAwesomeIcon icon={faCircleXmark} />
+                <Tippy
+                    interactive
+                    visible={searchResult.length > 0}
+                    render={attrs => (
+                        <div className={classes['search-result']} tabIndex="-1" {...attrs}>
+                            <PopperWrapper>
+                                <h4 className={classes['search-title']}>
+                                    FOOD
+                                </h4>
+                                <FoodItem />
+                                <FoodItem />
+                                <FoodItem />
+                                <FoodItem />
+                            </PopperWrapper>
+                        </div>
+                    )}
+                >
+                    <div className={classes.search}>
+                        <input placeholder="Search dishes..." spellCheck={false} />
+                        <button className={classes.clear}>
+                            <FontAwesomeIcon icon={faCircleXmark} />
 
-                    </button>
-                    <FontAwesomeIcon className={classes.loading} icon={faSpinner} />
+                        </button>
+                        <FontAwesomeIcon className={classes.loading} icon={faSpinner} />
 
-                    <button className={classes['search-btn']}>
-                        <FontAwesomeIcon icon={faMagnifyingGlass} />
 
-                    </button>
-                </div>
+                        <button className={classes['search-btn']}>
+                            <FontAwesomeIcon icon={faMagnifyingGlass} />
+                        </button>
+                    </div>
+                </Tippy>
 
                 <div className={classes.actions}>
                     <ul className={classes['menu-list']}>
