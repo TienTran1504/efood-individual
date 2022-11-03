586a209ebdff73bcd05347f1c0d4e2a56dc3ced9
TienTran1504 <74586033+TienTran1504@users.noreply.github.com>
2022-11-03 16:02:52 +0700

    Header 2 ( adding FontAweSome )

--- src/components/Layout/DefaultLayout/Header/index.js
+++ src/components/Layout/DefaultLayout/Header/index.js
@@ -1,21 +1,45 @@
-import './style.css'
+import classes from './Header.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
+
+
 function Header() {
     return (
-        <div className="header">
-            <span class="label">
-                <span className="active">E</span>FOOD
-            </span>
+        <div className={classes.wrapper}>
 
-            <ul className="menu-list">
-                <li className="menu-item active">Home</li>
-                <li className="menu-item ">Menu</li>
-                <li className="menu-item ">Service</li>
-                <li className="menu-item ">Contact</li>
-            </ul>
+            <div className={classes.inner}>
+                <div className={classes.label}>
+                    <div className={classes.active}>E</div>FOOD
+                </div>
 
+                <div className={classes.search}>
+                    <input placeholder="Search dishes..." spellCheck={false} />
+                    <button className={classes.clear}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+
+                    </button>
+                    <FontAwesomeIcon className={classes.loading} icon={faSpinner} />
+
+                    <button className={classes['search-btn']}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+
+                    </button>
+                </div>
+
+                <div className={classes.actions}>
+                    <ul className={classes['menu-list']}>
+                        <li className={classes['menu-item']}><a href="/" className={classes.active}>Home</a></li>
+                        <li className={classes['menu-item']}><a href="/menu">Menu</a></li>
+                        <li className={classes['menu-item']}><a href="/service">Service</a></li>
+                        <li className={classes['menu-item']}><a href="/contact">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
         </div>
 
     );
 }
 
 export default Header;
+
+// để thêm hai class dùng template literal `${} ${}`
